3f8143272a78b1a30c1577915b3f158f92a3d1c4
Sean Parsons <git@futurenotfound.com>
2015-01-06 19:23:43 +0000

    Changed out IO for Identity.

--- src/Control/Monad/Trans/Reader/Wiring.hs
+++ src/Control/Monad/Trans/Reader/Wiring.hs
@@ -7,10 +7,11 @@ import Control.Monad.Trans.Reader
 
 -- $setup
 -- >>> import Control.Monad.Wiring
+-- >>> import Data.Functor.Identity
 -- >>> data Database1 = Database1
 -- >>> data Database2 = Database2
--- >>> let request1 userId = ReaderT $ (\database -> return ("User" ++ userId)) :: ReaderT Database1 IO String
--- >>> let request2 userId = ReaderT $ (\database -> return ("User" ++ userId)) :: ReaderT Database2 IO String
+-- >>> let request1 userId = ReaderT $ (\database -> return ("User" ++ userId)) :: ReaderT Database1 Identity String
+-- >>> let request2 userId = ReaderT $ (\database -> return ("User" ++ userId)) :: ReaderT Database2 Identity String
 
 -- | Promote from one ReaderT instance to another.
 --
@@ -18,9 +19,9 @@ import Control.Monad.Trans.Reader
 -- let composedResult = do
 --    result1 <- promoteReader $ request1 "1"
 --    result2 <- promoteReader $ request2 "2"
---    return [result1, result2] :: ReaderT (Database1, Database2) IO [String]
+--    return [result1, result2] :: ReaderT (Database1, Database2) Identity [String]
 -- :}
--- >>> runReaderT composedResult (Database1, Database2)
+-- >>> runIdentity $ runReaderT composedResult (Database1, Database2)
 -- ["User1", "User2"]
 promoteReader :: (Wirable r1 r2) => ReaderT r2 m a -> ReaderT r1 m a
 promoteReader = withReaderT wire
