0b268281c883b4024e77584226ef1ca1aad34043
Sean Parsons <git@futurenotfound.com>
2015-01-14 23:25:13 +0000

    Added the ability to elevate WriterT and ReaderT into RWST.

--- src/Control/Monad/Trans/Reader/Wiring.hs
+++ src/Control/Monad/Trans/Reader/Wiring.hs
@@ -6,6 +6,15 @@ module Control.Monad.Trans.Reader.Wiring(
 
 import Control.Monad.Wiring
 import Control.Monad.Trans.Reader
+import qualified Control.Monad.Trans.RWS.Lazy as RWSL
+import qualified Control.Monad.Trans.RWS.Strict as RWSS
+import Data.Monoid
 
-instance (Wirable r1 r2) => Wirable (ReaderT r2 m a) (ReaderT r1 m a) where
+instance (Wirable r1 r2) => Wirable (ReaderT r2 f a) (ReaderT r1 f a) where
   wire = withReaderT wire
+
+instance (Monoid w, Functor f) => Wirable (ReaderT r f a) (RWSL.RWST r w s f a) where
+  wire reader = RWSL.RWST $ (\r -> \s -> fmap (\a -> (a, s, mempty)) $ runReaderT reader $ r)
+
+instance (Monoid w, Functor f) => Wirable (ReaderT r f a) (RWSS.RWST r w s f a) where
+  wire reader = RWSS.RWST $ (\r -> \s -> fmap (\a -> (a, s, mempty)) $ runReaderT reader $ r)
--- src/Control/Monad/Trans/Writer/Lazy/Wiring.hs
+++ src/Control/Monad/Trans/Writer/Lazy/Wiring.hs
@@ -6,6 +6,14 @@ module Control.Monad.Trans.Writer.Lazy.Wiring(
 
 import Control.Monad.Wiring
 import Control.Monad.Trans.Writer.Lazy
+import qualified Control.Monad.Trans.RWS.Lazy as RWSL
+import qualified Control.Monad.Trans.RWS.Strict as RWSS
 
 instance (Wirable w1 w2, Functor f) => Wirable (WriterT w1 f a) (WriterT w2 f a) where
   wire = mapWriterT (fmap (\(a, w) -> (a, wire w)))
+
+instance (Functor f) => Wirable (WriterT w f a) (RWSL.RWST r w s f a) where
+  wire writer = RWSL.RWST $ (\r -> \s -> fmap (\(a, w) -> (a, s, w)) $ runWriterT writer)
+
+instance (Functor f) => Wirable (WriterT w f a) (RWSS.RWST r w s f a) where
+  wire writer = RWSS.RWST $ (\r -> \s -> fmap (\(a, w) -> (a, s, w)) $ runWriterT writer)
