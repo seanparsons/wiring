1fed0778a0e294327533f7bf863ee51671168049
Sean Parsons <git@futurenotfound.com>
2015-01-26 08:57:41 +0000

    Added example wiring.

--- test/Control/Monad/Trans/Reader/WiringSpec.hs
+++ test/Control/Monad/Trans/Reader/WiringSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, ScopedTypeVariables #-}
 
 module Control.Monad.Trans.Reader.WiringSpec where
 
@@ -12,6 +12,26 @@ import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Reader.Wiring
 import qualified Control.Monad.Trans.RWS.Lazy as RWSL
 import qualified Control.Monad.Trans.RWS.Strict as RWSS
+
+data Database1 = Database1 deriving Show
+data Resource1 = Resource1 deriving Show
+data Database2 = Database2 deriving Show
+data User = User String deriving Show
+
+describeOrders :: User -> [String] -> String
+describeOrders user orders = "User " ++ (show user) ++ " ordered " ++ (show orders)
+
+userLookup :: Int -> ReaderT (Resource1, Database1) Identity User
+userLookup userId = ReaderT (\_ -> Identity $ User ("testuser" ++ show userId))
+
+ordersLookup :: Int -> ReaderT (Database2, Resource1) Identity [String]
+ordersLookup userId = ReaderT (\_ -> Identity $ ["Cake"])
+
+composedLookup :: Int -> ReaderT (Resource1, Database1, Database2) Identity String
+composedLookup userId = do
+  user    <- wire $ userLookup userId
+  orders  <- wire $ ordersLookup userId
+  return $ describeOrders user orders
 
 spec :: Spec
 spec = do
@@ -34,3 +54,7 @@ spec = do
             promoted  = wire $ readerT :: RWSS.RWST Double String Double Identity String
             result    = runIdentity $ RWSS.runRWST promoted r s
         in  result `shouldBe` (show r, s, mempty))
+    prop "Example of basic reader wiring" $ do
+      (\userId -> 
+        let result = runIdentity $ runReaderT (composedLookup userId) (Resource1, Database1, Database2)
+        in  result `shouldBe` (describeOrders (User ("testuser" ++ show userId)) ["Cake"]))
