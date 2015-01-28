c4f5044948ccc89a9acd74469362659db3aa977d
Sean Parsons <git@futurenotfound.com>
2015-01-28 08:47:11 +0000

    Add in pointless newline.

--- src/Control/Monad/Trans/RWS/Strict/Wiring.hs
+++ src/Control/Monad/Trans/RWS/Strict/Wiring.hs
@@ -9,3 +9,4 @@ import Control.Monad.Trans.RWS.Strict
 
 instance (Wirable w1 w2, Wirable r1 r2, Functor f) => Wirable (RWST r2 w1 s f a) (RWST r1 w2 s f a) where
   wire = withRWST (\r -> \s -> (wire r, s)) . mapRWST (fmap (\(a, s, w) -> (a, s, wire w)))
+
