dd1e77a5b83be65739947c0487e13a1f0b92d97f
Sean Parsons <git@futurenotfound.com>
2015-02-04 19:12:02 +0000

    Added some docs.

--- src/Control/Monad/Wiring/Types.hs
+++ src/Control/Monad/Wiring/Types.hs
@@ -6,10 +6,13 @@ module Control.Monad.Wiring.Types(
   ) where
 
 class Wirable a b where
+  -- | Unambiguous conversion from a to b.
   wire :: a -> b
 
+-- | The "identity" Wirable instance.
 instance Wirable a a where
   wire a = a
 
+-- | Unit can be obtained from anything.
 instance Wirable a () where
   wire _ = ()
