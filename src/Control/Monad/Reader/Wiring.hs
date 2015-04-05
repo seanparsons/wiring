f970ec4d93db9d72a6cc2e827e19b353f8ce449d
Sean Parsons <git@futurenotfound.com>
2015-04-05 17:42:33 +0100

    Experimenting with ask.

--- src/Control/Monad/Reader/Wiring.hs
+++ src/Control/Monad/Reader/Wiring.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module Control.Monad.Reader.Wiring(
-  Wirable(..)
+  Wirable(..),
+  wiredAsk
 ) where
 
 import Control.Monad.Wiring
 import Control.Monad.Reader
+import Control.Monad.Reader.Class
 import qualified Control.Monad.RWS.Lazy as RWSL
 import qualified Control.Monad.RWS.Strict as RWSS
 import Data.Monoid
@@ -18,3 +20,6 @@ instance (Monoid w, Functor f, Wirable r1 r2) => Wirable (ReaderT r2 f a) (RWSL.
 
 instance (Monoid w, Functor f, Wirable r1 r2) => Wirable (ReaderT r2 f a) (RWSS.RWST r1 w s f a) where
   wire reader = RWSS.RWST $ (\r -> \s -> fmap (\a -> (a, s, mempty)) $ runReaderT (withReaderT wire reader) r)
+
+wiredAsk :: (MonadReader r1 m, Wirable r1 r2) => m r2
+wiredAsk = fmap wire ask
